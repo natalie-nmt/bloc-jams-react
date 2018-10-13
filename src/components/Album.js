2a8859d75edcc61d066a465ac3d4eecb76d78646
Natalie Thomas <natalie.thomas.nmt@gmail.com>
2018-10-12 21:28:04 -0400

    Added audio playback start, pause, and switch song functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,7 +11,37 @@ import albumData from './../data/albums';
 
      this.state = {
        album: album,
+       currentSong: album.songs[0],
+       isPlaying: false
      };   
+      
+     this.audioElement = document.createElement('audio');
+     this.audioElement.src = album.songs[0].audioSrc;
+}
+
+play() {
+  this.audioElement.play();
+  this.setState({ isPlaying: true });
+}
+
+pause() {
+  this.audioElement.pause();
+  this.setState({ isPlaying: false });
+}   
+
+setSong(song) {
+  this.audioElement.src = song.audioSrc;
+  this.setState({ currentSong: song });
+}
+
+handleSongClick(song) {
+  const isSameSong = this.state.currentSong === song;
+  if (this.state.isPlaying && isSameSong) {
+    this.pause();
+  } else {
+    if (!isSameSong) { this.setSong(song); }     
+    this.play();
+  }
 }
  
    render() {
@@ -33,7 +63,7 @@ import albumData from './../data/albums';
            </colgroup>  
            <tbody>
                {this.state.album.songs.map( (song, index) =>
-                <tr key={index}>
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                   <td>{index + 1}</td>
                   <td>{song.title}</td>
                   <td>{song.duration} seconds</td>
